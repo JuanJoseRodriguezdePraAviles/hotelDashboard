ebab795fdfbc788aff3115889cc50db9c27e3f5c
JuanJoseRodriguezdePraAviles <45094901+JuanJoseRodriguezdePraAviles@users.noreply.github.com>
2025-04-04 09:48:27 +0200

    room not fully implemented

--- src/pages/RoomList/NewRoom.jsx
+++ src/pages/RoomList/NewRoom.jsx
@@ -8,16 +8,16 @@ export const NewRoom = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [formData, setFormData] = useState({
-        client_id: '',
-        client_name: '',
-        client_email: '',
-        client_phone: '',
-        check_in_date: '',
-        check_out_date: '',
-        special_request: '',
         room_id: '',
         room_type: '',
-        status: 'booked'
+        room_floor: '',
+        status: '',
+        description: '',
+        photos: [],
+        offer: false,
+        price: 0,
+        discount: 0,
+        cancellation_policy: ''
     });
 
 
@@ -54,58 +54,9 @@ export const NewRoom = () => {
     return (
         <NewRoomWrapper>
             <NewRoomTitle>New room</NewRoomTitle>
-            <Label>Client ID</Label>
-            <FieldText name="client_id" value={formData.client_id} onChange={handleChange} />
-            {errors.client_id &&
-                <ValidationError>
-                    {errors.client_id}
-                </ValidationError>
-            }
-            <Label>Client Name</Label>
-            <FieldText name="client_name" value={formData.client_name} onChange={handleChange} />
-            {errors.client_name &&
-                <ValidationError>
-                    {errors.client_name}
-                </ValidationError>
-            }
-            <Label>Client Email</Label>
-            <FieldText name="client_email" value={formData.client_email} onChange={handleChange} />
-            {errors.client_email &&
-                <ValidationError>
-                    {errors.client_email}
-                </ValidationError>
-            }
-            <Label>Client Phone</Label>
-            <FieldText name="client_phone" value={formData.client_phone} onChange={handleChange} />
-            {errors.client_name &&
-                <ValidationError>
-                    {errors.client_name}
-                </ValidationError>
-            }
-            <Label>Check In Date</Label>
-            <DateInput type="date" name="check_in_date" value={formData.check_in_date} onChange={handleChange} />
-            {errors.check_in_date &&
-                <ValidationError>
-                    {errors.check_in_date}
-                </ValidationError>
-            }
-            <Label>Check Out Date</Label>
-            <DateInput type="date" name="check_out_date" value={formData.check_out_date} onChange={handleChange} />
-            {errors.check_out_date &&
-                <ValidationError>
-                    {errors.check_out_date}
-                </ValidationError>
-            }
-            <Label>Special Request</Label>
-            <FieldText name="special_request" value={formData.special_request} onChange={handleChange} />
-            {errors.special_request &&
-                <ValidationError>
-                    {errors.special_request}
-                </ValidationError>
-            }
             <Label>Room ID</Label>
             <FieldText name="room_id" value={formData.room_id} onChange={handleChange} />
-            {errors.room_id &&
+            {errors.client_id &&
                 <ValidationError>
                     {errors.room_id}
                 </ValidationError>
@@ -115,6 +66,48 @@ export const NewRoom = () => {
             {errors.room_type &&
                 <ValidationError>
                     {errors.room_type}
+                </ValidationError>
+            }
+            <Label>Room Floor</Label>
+            <FieldText name="room_floor" value={formData.room_floor} onChange={handleChange} />
+            {errors.room_floor &&
+                <ValidationError>
+                    {errors.room_floor}
+                </ValidationError>
+            }
+            <Label>Status:</Label>
+            <FieldText name="status" value={formData.status} onChange={handleChange} />
+            {errors.status &&
+                <ValidationError>
+                    {errors.status}
+                </ValidationError>
+            }
+            <Label>Description:</Label>
+            <FieldText name="description" value={formData.description} onChange={handleChange} />
+            {errors.description &&
+                <ValidationError>
+                    {errors.description}
+                </ValidationError>
+            }
+            <Label>Price: </Label>
+            <FieldText type="number" name="price" value={formData.price} onChange={handleChange} />
+            {errors.price &&
+                <ValidationError>
+                    {errors.price}
+                </ValidationError>
+            }
+            <Label>Discount: </Label>
+            <FieldText type="number" name="discount" value={formData.discount} onChange={handleChange} />
+            {errors.discount &&
+                <ValidationError>
+                    {errors.discount}
+                </ValidationError>
+            }
+            <Label>Cancellation Policy:</Label>
+            <FieldText name="cancellation_policy" value={formData.cancellation_policy} onChange={handleChange} />
+            {errors.cancellation_policy &&
+                <ValidationError>
+                    {errors.cancellation_policy}
                 </ValidationError>
             }
             <SubmitBtn onClick={handleSubmit}>Submit</SubmitBtn>

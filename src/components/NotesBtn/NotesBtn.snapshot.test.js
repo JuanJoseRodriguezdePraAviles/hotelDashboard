b8b573824d51477bfbce7ba528e2bc4f5feeef51
JuanJoseRodriguezdePraAviles <45094901+JuanJoseRodriguezdePraAviles@users.noreply.github.com>
2025-04-10 12:07:17 +0200

    Bug Fix: fixed component test for button

--- src/components/NotesBtn/NotesBtn.snapshot.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import NotesBtn from "./NotesBtn";
-
-test("renders View Notes button", async () => {
-    render(<NotesBtn value="View Notes"/>);
-    const button = await screen.findByText(/View Notes/i);
-    expect(button).toBeInTheDocument();
-});
--- src/components/NotesBtn/NotesBtn.test.js
+++ src/components/NotesBtn/NotesBtn.test.js
@@ -3,26 +3,9 @@ import React from 'react';
 import { NotesBtn } from "./NotesBtn.jsx";
 
 describe("NotesBtn", () => {
-    let originalFetch;
-
-    beforeEach(() => {
-        originalFetch = global.fetch;
-        global.fetch = jest.fn(() =>
-            Promise.resolve([
-                {
-                    json: () => Promise.resolve([{ value: "View Notes"}])
-                }
-            ])
-        );
-    });
-
-    afterEach(() => {
-        global.fetch = originalFetch;
-    });
-
     it("render NotesBtn component", async () => {
         render(<NotesBtn value="View Notes"/>);
         const valueElement = await screen.findByText(/View Notes/i);
-        expect(valueElement).toBeInTheDocument();
+        expect(valueElement).toHaveProperty('textContent', 'View Notes');
     });
 });
